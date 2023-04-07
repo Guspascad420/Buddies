c85be7ab2767b96363e4db8e24ff8c5adefc022d
Guspascad420 <Paacad@gmail.com>
2023-04-08 00:29:04 +0700

    Added Search results in form of Card

--- app/src/main/java/com/example/collpage/ui/screens/SearchPage.kt
+++ app/src/main/java/com/example/collpage/ui/screens/SearchPage.kt
@@ -1,6 +1,11 @@
 package com.example.collpage.ui.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -22,7 +27,10 @@ fun SearchPage(
     viewModel: SearchViewModel = viewModel(),
     navigateToFilter: () -> Unit
 ) {
-    Column(Modifier.padding(horizontal = 8.dp).padding(top = 20.dp, bottom = 12.dp)) {
+    Column(
+        Modifier
+            .padding(horizontal = 8.dp)
+            .padding(top = 20.dp, bottom = 12.dp)) {
         Row(Modifier.fillMaxWidth(), Arrangement.SpaceAround) {
             Box(Modifier.padding(top = 7.dp)) {
                 IconButton(onClick = { /*TODO*/ }) {
@@ -32,7 +40,9 @@ fun SearchPage(
             OutlinedTextField(
                 viewModel.query,
                 { viewModel.query = it },
-                Modifier.width(280.dp).padding(end = 10.dp),
+                Modifier
+                    .width(280.dp)
+                    .padding(end = 10.dp),
                 shape = RoundedCornerShape(35.dp),
                 colors = TextFieldDefaults.outlinedTextFieldColors(
                     backgroundColor = getInputColor(),
@@ -53,6 +63,105 @@ fun SearchPage(
                 }
             }
         }
-        Text("Riwayat", fontFamily = Poppins, fontWeight = FontWeight.SemiBold, fontSize = 24.sp)
+        Spacer(Modifier.height(15.dp))
+        Text("Riwayat", Modifier.padding(start = 10.dp), fontFamily = Poppins,
+            fontWeight = FontWeight.SemiBold, fontSize = 24.sp)
+        SearchResult(viewModel = viewModel)
+    }
+}
+
+@Composable
+fun SearchResult(viewModel: SearchViewModel) {
+    val resultTypeList = listOf("Semua", "Akun", "Pekerjaan", "Kursus")
+    LazyColumn() {
+        item {
+            LazyRow {
+                items(resultTypeList) { type ->
+                    val borderWidth = if (viewModel.activeResultType == type) 1.dp else 0.dp
+                    val bgColor = if (viewModel.activeResultType == type) Color(0xFF1C6973)
+                    else MaterialTheme.colors.background
+                    Button(
+                        onClick = {  },
+                        Modifier.padding(end = 10.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            backgroundColor = bgColor
+                        ),
+                        border = BorderStroke(borderWidth, MaterialTheme.colors.onSurface),
+                        shape = RoundedCornerShape(15.dp)
+                    ) {
+                        Text(
+                            type, Modifier.padding(5.dp),
+                            fontFamily = Poppins
+                        )
+                    }
+                }
+            }
+        }
+        item {
+            CardResult()
+        }
+    }
+}
+
+@Composable
+fun CardResult() {
+    Card(Modifier.padding(10.dp), RoundedCornerShape(18.dp)) {
+        Column(Modifier.padding(20.dp)) {
+            Row {
+                Image(
+                    painterResource(R.drawable.figmavektor), null,
+                    Modifier.padding(top = 5.dp, end = 8.dp)
+                )
+                Column {
+                    Text(
+                        "UI/UX Design with Figma",
+                        fontFamily = Poppins,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                    Text(
+                        "Progate",
+                        Modifier.padding(horizontal = 20.dp),
+                        fontFamily = Poppins,
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Light,
+                        color = Color(0xFF909090)
+                    )
+                    //ini gimana buat yang nilainya
+                }
+            }
+            Divider(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 15.dp, vertical = 7.dp))
+            Row(modifier = Modifier.fillMaxWidth(), Arrangement.SpaceBetween) {
+                Row {
+                    Image(
+                        painterResource(R.drawable.rp), null,
+                        Modifier.padding(top = 5.dp, end = 5.dp)
+                    )
+                    Text(
+                        "Rp. 150.000",
+                        fontFamily = Poppins,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                }
+                Button(
+                    onClick = { },
+                    shape = RoundedCornerShape(15.dp),
+                    colors = ButtonDefaults.buttonColors(Color(0xFF1C6973)),
+                    modifier = Modifier
+                ) {
+                    Text(
+                        "Detail",
+                        fontFamily = Poppins,
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 20.sp,
+                        color = Color.White,
+                    )
+                }
+            }
+        }
     }
 }
